6fcd136c912de6ef1a9afb86c3b93dd439b373ba
benjaminRoot23 <benjamin.ponce@masuno.pe>
2022-11-26 17:57:48 -0500

    variables de entorno

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import { API_URL } from '../env'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      <h1>Hola Mundo {import.meta.env.VITE_API_URL}</h1>
+      <p>desde env.js {API_URL}</p>
     </div>
   )
 }
